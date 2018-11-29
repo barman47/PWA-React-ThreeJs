74420ba7b6dc48f47e7f45643daf687cc8d43946
Nathan <nathanfrad86@gmail.com>
2018-11-29 09:40:38 +0100

    custom service worker de workbox

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { updatePresent: "non" };
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            aller la .
-          </p>
 
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Si je modifie  .
+                    </p>
+
+                    <p> updatePresent : {this.state.updatePresent} </p>
+
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,31 +4,9 @@ import './index.css';
 import App from './App';
 import * as registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-
-// function displayNotification() {
-//     document
-//         .querySelector("#notification")
-//         .style.display = "block";
-// }
 
 registerServiceWorker.register();
-//
-// document.querySelector("#on-activation-request")
-//     .addEventListener("click", () => {
-//         // On récupère le Service Worker
-//         // qui a fini de s'installer
-//         // (waiting)
-//         navigator.serviceWorker
-//             .getRegistration()
-//             .then(registration => {
-//                 // Et on lui envoie le
-//                 // message d'activation
-//                 registration.waiting
-//                     .postMessage("skipWaiting");
-//             });
-//     });
+
+
