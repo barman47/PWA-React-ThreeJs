62dea20816c6b051aa8817672c82d94b57ddac9f
Nathan <nathanfrad86@gmail.com>
2018-11-29 15:03:55 +0100

    example three.js in react : ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import ThreeContainer from './component/ThreeContainer'
 
 
 class App extends Component {
@@ -9,32 +10,37 @@ class App extends Component {
         this.state = { updatePresent: "non" };
     }
 
-
-
-
-    render() {
+    render () {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Si je modifie  .
-                    </p>
-
-                    <p> updatePresent : {this.state.updatePresent} </p>
-
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
-            </div>
+            <ThreeContainer  />
         );
     }
+
+
+
+    // render() {
+    //     return (
+    //         <div className="App">
+    //             <header className="App-header">
+    //                 <img src={logo} className="App-logo" alt="logo"/>
+    //                 <p>
+    //                     Si je modifie  .
+    //                 </p>
+    //
+    //                 <p> updatePresent : {this.state.updatePresent} </p>
+    //
+    //                 <a
+    //                     className="App-link"
+    //                     href="https://reactjs.org"
+    //                     target="_blank"
+    //                     rel="noopener noreferrer"
+    //                 >
+    //                     Learn React
+    //                 </a>
+    //             </header>
+    //         </div>
+    //     );
+    // }
 }
 
 
